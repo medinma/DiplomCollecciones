cac92783df3fc92aee159743c59b7a224ed3e899
Martin Medina <Martin Medina>
2016-10-26 00:03:17 -0300

    primer commit

---
+++ ejercicio1/Banco.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace operacionesBancarias.dominio
+{
+    /*
+    * La clase Banco implementa el patrón de diseño Singleton
+    * porque sólo puede haber un objeto del tipo Banco
+    */
+    class Banco
+    {
+        /*
+        * La variable de referencia qua almacena la instancia de Banco.
+        */
+        private static Banco instanciaBanco = new Banco();
+        public static Banco getBanco()
+        {
+            return instanciaBanco;
+        }
+
+        private int MAX_CLIENTES = 10;
+        private const double PORCENTAJE_CAJA_AHORRO = 3.5;
+        private Cliente[] clientes;
+        private int numeroDeClientes;
+
+        private Banco()
+        {
+            clientes = new Cliente[MAX_CLIENTES];
+            numeroDeClientes = 0;
+        }
+
+        public void AgregaCliente(String p, String a)
+        {
+            int i = numeroDeClientes++;
+            clientes[i] = new Cliente(p, a);
+        }
+        public Cliente GetCliente(int indiceCliente)
+        {
+            return clientes[indiceCliente];
+        }
+        public int NumeroDeClientes
+        {
+            get
+            {
+                return numeroDeClientes;
+            }
+        }
+    }
+}
---
+++ ejercicio1/Cliente.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace operacionesBancarias.dominio
+{
+    class Cliente
+    {
+        // Atributos para datos
+        private String primerNombre;
+        private String apellido;
+        // Atributos para la agregación
+        private Cuenta[] cuentas = new Cuenta[5];
+        private int numeroDeCuentas = 0;
+
+        public Cliente(String p, String a)
+        {
+            primerNombre = p;
+            apellido = a;
+        }
+
+        public void AgregaCuenta(Cuenta cta)
+        {
+            int i = numeroDeCuentas++;
+            cuentas[i] = cta;
+        }
+        public Cuenta GetCuenta(int indiceCuenta)
+        {
+            return cuentas[indiceCuenta];
+        }
+
+        public String PrimerNombre
+        {
+            get
+            {
+                return primerNombre;
+            }
+        }
+        public String Apellido
+        {
+            get
+            {
+                return apellido;
+            }
+        }
+        public int NumeroDeCuentas
+        {
+            get
+            {
+                return numeroDeCuentas;
+            }
+        }
+    }
+}
